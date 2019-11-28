5dba5ed9c23276e2463e050232917710c7e83c04
antonina <antoninasych@gmail.com>
2019-11-28 19:49:29 +0200

    updated according issues

--- js/slider.js
+++ js/slider.js
@@ -34,56 +34,73 @@ class Slider {
                 "note": "* At vero eos et accusamus et iusto odo dtgntsslmos duclmus qui blandltlis praesentlum voluptatum delenrtl atque corruptl quos doQres et quas molestlas exceptun sint occaecatl cupidrtate non pro v dent, slmllique sunt In culpa qui otflcia deserunt mollrtia anlmi. id est la bo aim et dolorum tuga.",
                 "productUrl": "https://res.cloudinary.com/dx4wkpab8/image/upload/v1573640171/comp_plate_promo4_f87x7g.png"
             }
-        ]; //хотелось разобраться с async await поэтому так долго
-
-
+        ];
+        this.toggle = false;
+        this.wrapper = document.getElementById(this.sliderWrapperId);
+        this.switchSlides = 0;
     }
 
     drawSlider() {
         document.getElementById(this.sliderWrapperId).innerHTML = '';
-
         this.activeSlide = this.activeSlide === null ? 0 : this.activeSlide;
-        console.log(this.activeSlide);
-        const wrapper = document.getElementById(this.sliderWrapperId);
-        let html = '<div class="slides">';
-        for (var i = 0; i < this.data.length; i++) {
+        let toogleClass = (this.toggle === true) ? 'show-toogle' : 'hide-toogle';
+        let html = `<div class="slides">`;
+        let i = 0;
+        this.data.forEach(function (slide) {
             let activeClass = this.activeSlide === i ? 'show' : 'hide';
+            html += `<div class="slide   ${activeClass}" id="slide-${i}" >`;
+            html += `<div class="img"><img src="${slide.img}"></div>`;
+            html += `<div class="title">${slide.title} </div>`;
+            html += `<div class="description ${toogleClass}" >  <p>${slide.description}</p></div>`;
+            html += `</div>`;
+            i++;
+        }, this);
 
-            html += '<div class="slide ' + activeClass + ' " >';
-            html += '<div class="img"><img src="' + this.data[i]['img'] + '"></div>';
-            html += '<div class="title">' + this.data[i]['title'] + ' </div>';
-
-
-            html += '</div>';
-        }
+        html += `</div>`;
+        html += `<div class="buttons bottom right">` +
+            `<div class="button" id="toogle"> toggle</div>` +
+            `<div class="button" id="prev"><<< Prev</div>` +
+            `<div class="button" id="next">Next >>></div>`;
         html += '</div>';
-        html += '<div class="buttons bottom right"> ' +
-            '<div class="button" id="prev"><<< Prev</div>' +
-            '<div class="button" id="next">Next >>></div>';
-
-
-        html += '</div>';
-        wrapper.insertAdjacentHTML("afterbegin", html);
-
+        this.wrapper.innerHTML = html;
         this.addEventListener();
     }
 
     addEventListener() {
-        document.getElementById('prev').addEventListener('click', this.prev.bind(this), false);
-        document.getElementById('next').addEventListener('click', this.next.bind(this), false);
+        let prevBtn = document.querySelector("#prev");
+        let nextBtn = document.querySelector("#next");
+        let toogleBtn = document.querySelector("#toogle");
+
+        prevBtn.addEventListener('click', () => this.prev(), false);
+        nextBtn.addEventListener('click', () => this.next(), false);
+        toogleBtn.addEventListener('click', () => this.toogler(), false);
     }
 
-    next(obj) {
-        this.activeSlide = this.activeSlide < this.data.length - 1 ? this.activeSlide = this.activeSlide + 1 : this.activeSlide = 0;
+    next() {
+        this.switchSlides = this.activeSlide;
+        this.activeSlide = this.activeSlide < this.data.length - 1 ? this.activeSlide + 1 : 0;
+        this.changeSlide();
+    }
+
+    prev() {
+        this.switchSlides = this.activeSlide;
+        this.activeSlide = this.activeSlide === 0 ? this.data.length - 1 : this.activeSlide - 1;
+        this.changeSlide();
+    }
+
+    toogler() {
+        this.toggle = !this.toggle;
         this.drawSlider();
-
     }
 
-    prev(obj) {
-        this.activeSlide = this.activeSlide === 0 ? this.activeSlide = this.data.length - 1 : this.activeSlide = this.activeSlide - 1;
-        this.drawSlider();
-    }
-
+    changeSlide() {
+        let previeusElement = document.querySelector(`#slide-${this.switchSlides}`);
+        let activeElement = document.querySelector(`#slide-${this.activeSlide}`);
+        previeusElement.classList.remove("show");
+        previeusElement.classList.add("hide");
+        activeElement.classList.remove("hide");
+        previeusElement.classList.add("show");
+     }
 }
 
 
